629a3726ad527fa06a44738a5f3712e53dcb1775
Ramakanth <ramakanth.chennur@fissionlabs.com>
2023-04-04 14:10:52 +0530

    initial commit about sortable cards react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SortableCards from "./Components/SortableCards";
+
+function App() {
+  return (
+    <div className="App">
+      <SortableCards />
+    </div>
+  );
+}
+
+export default App;
